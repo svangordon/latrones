113e6cde16a54731391d012fe6468d2eb431520d
sophie <stephen.van.gordon@gmail.com>
2017-03-01 12:04:08 -0700

    basic CRD for users (UPDATE unecessary as it's all still pretty much a stub)

--- constants.py
+++ constants.py
@@ -1,16 +1,17 @@
 db = {
-    "users": "users"
+    "users": "user"
 }
 
 sql_templates = {
-    "create_user": """INSERT INTO {users_table} (username)
-    SELECT * FROM (SELECT '{username}') AS tmp
-    WHERE NOT EXISTS(
-	   SELECT username FROM {users_table} WHERE username = '{username}'
-   ) LIMIT 1;
-   """,
+    "create_user": """INSERT INTO {users_table} (username) VALUES ('{username}');""",
    "get_username": """SELECT * FROM {users_table} WHERE username = '{username}'""",
    "get_user": """SELECT * FROM {users_table} WHERE user_id = {user_id}""",
-   "delete_user": """DELETE FROM {users_table} WHERE user_id = {user_id}"""
-
+   "delete_user": """DELETE FROM {users_table} WHERE user_id = {user_id}""",
+   "user": {
+       "create_user": """INSERT INTO {users_table} (username) VALUES ('{username}');""",
+       "get_username": """SELECT * FROM {users_table} WHERE username = '{username}'""",
+        "get_user": """SELECT * FROM {users_table} WHERE user_id = {user_id}""",
+        "delete_user": """DELETE FROM {users_table} WHERE user_id = {user_id}""",
+        "get_all": """SELECT * FROM {users_table}"""
+   }
 }
--- latr.py
+++ latr.py
@@ -9,27 +9,17 @@ from Users import get_user
 
 cnx = MySQLdb.connect(host="localhost", user="igoinu", passwd="password", db="latr")
 
-def db_wrapper(script, resp_flag):
-    """ simple wrapper to execute MySQLdb queries and return one or more rows of results """
+###
+# User Handlers
+###
+
+@hug.get('/user')
+def get_all_users():
     cur = cnx.cursor()
-    try:
-        cur.execute(script)
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: %s" % str(e))
-    try:
-        if int(resp_flag) == 1:
-            return cur.fetchone()
-        elif int(resp_flag) > 0:
-            return cur.fetchmany(int(resp_flag))
-        return
-    except TypeError:
-        # resp_flag == None, and we're done
-        return
-    except ValueError:
-        return cur.fetchmany()
+    cur.execute(sql_templates["user"]["get_all"].format(users_table=constants.db["users"]))
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 
 @hug.get('/user/{user_id}')
 def get_user_handler(user_id):
@@ -39,13 +29,16 @@ def get_user_handler(user_id):
     cur.close()
     return row
 
-# print(db_wrapper(get_user(13)))
-# print(get_user_handler(13))
+
 @hug.post('/user')
 def create_user_handler(username):
     cur = cnx.cursor()
-    cur.execute(sql_templates["create_user"].format(username=username, users_table=constants.db["users"]))
-    cnx.commit()
+    try:
+        cur.execute(sql_templates["create_user"].format(username=username, users_table=constants.db["users"]))
+        cnx.commit()
+    except MySQLdb.Error as e:
+        return "Username may already exist"
+
     cur.execute(sql_templates["get_username"].format(username=username, users_table=constants.db["users"]))
     row = cur.fetchone()
     cur.close()
@@ -53,9 +46,26 @@ def create_user_handler(username):
 
 @hug.delete('/user/{user_id}')
 def delete_user_handler(user_id):
+    cur = cnx.cursor()
     cur.execute(sql_templates["delete_user"].format(user_id=user_id, users_table=constants.db["users"]))
-    db.commit()
+    cnx.commit()
+    cur.close()
     return "Operation successful"
 
-# @hug.post('/game')
-# def new_game_handler()
+###
+# Game Handlers
+###
+@hug.post('/game')
+def create_game_handler(game_width, game_height, user_id):
+    """ Create a new game """
+    # Eventually, do some kind of matchmaking here.
+    # For now, just return 1
+
+@hug.get('/game')
+def get_games_handler(user_id):
+    """ Return the id's of all games a player is a participant in """
+    cur = cnx.cursor()
+    cur.execute(sql_templates["get_games"].format(user_id=user_id, users_table=constants.db["users"]))
+    row = cur.fetchall()
+    cur.close()
+    return row
