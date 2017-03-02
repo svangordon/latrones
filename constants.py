553b8ac0d844f3e6022708df5e8e87517231b508
sophie <stephen.van.gordon@gmail.com>
2017-03-02 09:53:07 -0700

    working on backend

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 db = {
-    "users": "user"
+    "users": "user",
+    "game": "game",
+    "participant": "participant"
 }
 
 sql_templates = {
@@ -13,5 +15,17 @@ sql_templates = {
         "get_user": """SELECT * FROM {users_table} WHERE user_id = {user_id}""",
         "delete_user": """DELETE FROM {users_table} WHERE user_id = {user_id}""",
         "get_all": """SELECT * FROM {users_table}"""
-   }
+   },
+   "game": {
+        "create_game": """INSERT INTO {game_table} (board_width, board_height) VALUES ({board_width}, {board_height})""",
+        "delete_game": """DELETE FROM {game_table} WHERE game_id = {game_id}""",
+        "get_all_games": """SELECT * FROM {game_table}"""
+   },
+   "participant": {
+        "create_participant": """INSERT INTO {participant_table} (game_id, user_id) VALUES ({game_id}, {user_id})""",
+        "get_participant_games": """SELECT * FROM {participant_table} where user_id = {user_id}"""
+    },
+    "util": {
+        "last_insert": """SELECT LAST_INSERT_ID();"""
+    }
 }
--- latr.py
+++ latr.py
@@ -37,7 +37,7 @@ def create_user_handler(username):
         cur.execute(sql_templates["create_user"].format(username=username, users_table=constants.db["users"]))
         cnx.commit()
     except MySQLdb.Error as e:
-        return "Username may already exist"
+        return "Could not add username. {0}".format(e)
 
     cur.execute(sql_templates["get_username"].format(username=username, users_table=constants.db["users"]))
     row = cur.fetchone()
@@ -47,25 +47,53 @@ def create_user_handler(username):
 @hug.delete('/user/{user_id}')
 def delete_user_handler(user_id):
     cur = cnx.cursor()
-    cur.execute(sql_templates["delete_user"].format(user_id=user_id, users_table=constants.db["users"]))
-    cnx.commit()
+    try:
+        cur.execute(sql_templates["delete_user"].format(user_id=user_id, users_table=constants.db["users"]))
+        cnx.commit()
+    except MySQLdb.Error as e:
+        return "Could not delete user. {0}".format(e[0])
     cur.close()
     return "Operation successful"
 
 ###
 # Game Handlers
 ###
-@hug.post('/game')
-def create_game_handler(game_width, game_height, user_id):
+@hug.post('/game/create', output=hug.output_format.json)
+def create_game_handler(user_id, board_width, board_height):
     """ Create a new game """
-    # Eventually, do some kind of matchmaking here.
-    # For now, just return 1
+    cur = cnx.cursor()
+    cur.execute(sql_templates["game"]["create_game"].format(board_width=board_width, board_height=board_height, game_table=constants.db["game"]))
+    cur.execute(sql_templates["util"]["last_insert"])
+    game_id = cur.fetchone()[0]
+    cur.execute(sql_templates["participant"]["create_participant"].format(user_id=user_id, game_id=game_id, participant_table=constants.db["participant"]))
+    cnx.commit()
+    # row = cur.fetchone()
+    cur.close()
+    return {"status": "success", "game_id": game_id}
 
-@hug.get('/game')
+@hug.get('/game/get/{user_id}')
 def get_games_handler(user_id):
     """ Return the id's of all games a player is a participant in """
     cur = cnx.cursor()
-    cur.execute(sql_templates["get_games"].format(user_id=user_id, users_table=constants.db["users"]))
+    cur.execute(sql_templates["participant"]["get_participant_games"].format(user_id=user_id, participant_table=constants.db["participant"]))
     row = cur.fetchall()
     cur.close()
     return row
+
+@hug.get('/games')
+def get_all_games_handler():
+    cur = cnx.cursor()
+    cur.execute(sql_templates["game"]["get_all_games"].format(game_table=constants.db["game"]))
+    row = cur.fetchall()
+    cur.close()
+    return row
+
+@hug.delete('/game/{game_id}')
+def delete_game_handler():
+    cur = cnx.cursor()
+    try:
+        cur.execute(sql_templates["delete_game"].format(game_id=game_id, game_table=constants.db["game"]))
+    except MySQLdb.Error as e:
+        return "Could not delete game. {0}".format(e[0])
+    cur.close()
+    return "Game deleted"
