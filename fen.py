5c088c061a2aca9141ad6e6adc3f317bd4c43e36
svangordon <stephen.van.gordon@gmail.com>
2017-03-02 18:52:31 -0700

    include char_generator

--- fen.py
+++ fen.py
@@ -6,3 +6,46 @@ def create_fen(board_width, board_height):
     # [position string] [boardWidth] [boardHeight] [activePlayer in w|b] [stoneCount] [gamePhase in d|m] [halfmoveClock] [fullMoveClock]
     position_string = '/'.join([str(board_width)] * board_height)
     return Template("$position_string $board_width $board_height w 0 d 0 1").substitute(locals())
+
+# def deserialize_fen(fen_input:str):
+#     """ Put the game in a python-parsable state"""
+#     fen_dict = dict(zip(('boardString', 'boardWidth', 'boardHeight', 'activePlayer', 'stoneCount', 'gamePhase', 'halfmoveClock', 'fullMoveClock') ,fen_input.split(' ')))
+#     # input_board = fen.split(' ')[0].split('/')
+#     output_board = [[]]
+#     for row in fen_dict["boardString"].split('/'):
+#         for char in row:
+#             if
+#
+# def deserialize_row(row_input):
+#     output_row = []
+#     for char in row_input:
+#         output_row.extend(deserialize_char(char))
+#     return output_row
+#
+# def deserialize_char(char_input):
+#     try:
+
+def char_generator(char=None):
+    """ Pass -1 for non-valid squares """
+    output = {
+        "occupied": False,
+        "checked": False,
+        "owner": -1,
+        "valid": False
+    }
+    if char == -1:
+        return output
+    output["valid"] = True
+    if char == None:
+        return output
+    output["occupied"] = True
+    if char.lower() == 'w':
+        output["owner"] = 0
+    elif char.lower() == 'b':
+        output["owner"] = 1
+    else:
+        raise ValueError('invalid char')
+
+    if char.isupper():
+        output["checked"] = True
+    return output
--- fen_tests.py
+++ fen_tests.py
@@ -10,5 +10,20 @@ class TestCreateFen(unittest.TestCase):
     def test_value(self):
         self.assertEqual(fen.create_fen(self.board_width, self.board_height), self.expected)
 
+class TestCharGenerator(unittest.TestCase):
+    def setUp(self):
+        self.input = [-1, 'w', 'W', 'b', 'B', None]
+        self.expected = [
+        {'owner': -1, 'occupied': False, 'valid': False, 'checked': False},
+        {'owner': 0, 'valid': True, 'occupied': True, 'checked': False},
+        {'valid': True, 'occupied': True, 'owner': 0, 'checked': True},
+        {'owner': 1, 'valid': True, 'checked': False, 'occupied': True},
+        {'checked': True, 'valid': True, 'occupied': True, 'owner': 1},
+        {'owner': -1, 'valid': True, 'checked': False, 'occupied': False}
+        ]
+
+    def test_values(self):
+        self.assertListEqual(self.expected, list(map(fen.char_generator, self.input)))
+
 if __name__ == '__main__':
     unittest.main()
