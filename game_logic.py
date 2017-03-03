ec6a471845f0054e2ed7e79c3343b3ea8f793bf2
svangordon <stephen.van.gordon@gmail.com>
2017-03-03 13:44:39 -0700

    include is_backwards fn

--- game_logic.py
+++ game_logic.py
@@ -20,9 +20,22 @@ def convert_point_to_alg(coord, board_width, board_height):
     row = int(coord / (board_width + 2))
     return ascii_lowercase[col] + str(row)
 
-def validate_move(game_object, move_from, move_to):
+def validate_move(game_object, move_from, move_to, jumps=None):
     """ Return Bool for whether a given move is legal """
     board_dim = (game_object["board_width"], game_object["board_height"])
     origin_point = convert_alg_to_point(move_from, *board_dim)
-    origin_square = game_object["board"]
-    # Check to make sure
+    origin_square = game_object["board"][origin_point]
+    dest_point = convert_alg_to_point(move_to, *board_dim)
+    dest_square = game_object["board"][dest_point]
+    # origin square should be owned by active player
+    if origin_square["owner"] != game_object["active_player"]:
+        return False
+
+    # dest square should be valid
+    if not dest_square["valid"]:
+        return False
+
+def is_backwards(move_start, move_end, active_player, board_width):
+    """ Return whether a given move is backwards. Only interested in whether it is a single step backwards """
+    step = -(board_width+2) if active_player == 0 else board_width+2
+    return move_start + step == move_end
--- game_logic_tests.py
+++ game_logic_tests.py
@@ -62,44 +62,54 @@ class TestSquareConversion(unittest.TestCase):
             with self.assertRaises(ValueError):
                 game_logic.convert_point_to_alg(test, board_width, board_height)
 
-class TestMoveValidation(unittest.TestClass):
+# class TestMoveValidation(unittest.TestClass):
+#     def setUp(self):
+#
+#     def test_simple_moves(self):
+#         white_fen = '12/12/12/12/12/12/4w7/12 12 8 w 0 0 1'
+#         white_dict = fen.deserialize_fen_string(self.one_piece_fen)
+#         white_test_dest = [('d6', True), ('c7', True), ('e7', True), ('d8', False), ('g6', False), ('a1', False)]
+#         black_fen = '12/12/12/12/12/12/4b7/12 12 8 b 0 0 1'
+#         black_dict = fen.deserialize_fen_string(self.one_piece_fen)
+#         black_test_dest = [('d6', False), ('c7', True), ('e7', True), ('d8', True), ('g6', False), ('a1', False)]
+#         piece_start = 'd7'
+#         for test in white_test_dest:
+#             self.assertEqual(game_logic.validate_move(white_game_dict, piece_start, test[0]), test[1])
+#         for test in black_test_dest:
+#             self.assertEqual(game_logic.validate_move(black_game_dict, piece_start, test[0]), test[1])
+#
+#     def test_no_clip_moves(self):
+#         """ Test that can't clip enemy piece """
+#         white_fen = '12/1b10/12/12/12/4b7/3bwb6/4b7 12 8 w 0 0 1'
+#         white_dict = fen.deserialize_fen_string(self.white_fen)
+#         black_fen = '12/1w10/12/12/12/4w7/3wbw6/4w7 12 8 b 0 0 1'
+#         black_dict = fen.deserialize_fen_string(self.black_fen)
+#         move_squares = ['c7', 'd6', 'd8', 'e7']
+#         start_move = 'd7'
+#         for square in move_squares:
+#             self.assertFalse(game_logic.validate_move(white_dict, start_move, square))
+#             self.assertFalse(game_logic.validate_move(black_dict, start_move, square))
+#
+#     def test_move_own_only(self):
+#         """ Test that can't move opponent's pieces """
+#         white_fen = '12/12/12/12/12/12/4w7/12 12 8 b 0 0 1'
+#         black_fen = '12/12/12/12/12/12/4w7/12 12 8 w 0 0 1'
+#         white_dict = fen.deserialize_fen_string(white_fen)
+#         black_dict = fen.deserialize_fen_string(black_fen)
+#         start_move = 'd7'
+#         end_move = 'c7'
+#         self.assertFalse(game_logic.validate_move(white_dict, start_move, end_move))
+#         self.assertFalse(game_logic.validate_move(black_dict, start_move, end_move))
+
+class TestIsMoveBackwards(unittest.TestCase):
     def setUp(self):
-
-    def test_simple_moves(self):
-        white_fen = '12/12/12/12/12/12/4w7/12 12 8 w 0 0 1'
-        white_dict = fen.deserialize_fen_string(self.one_piece_fen)
-        white_test_dest = [('d6', True), ('c7', True), ('e7', True), ('d8', False), ('g6', False), ('a1', False)]
-        black_fen = '12/12/12/12/12/12/4b7/12 12 8 b 0 0 1'
-        black_dict = fen.deserialize_fen_string(self.one_piece_fen)
-        black_test_dest = [('d6', False), ('c7', True), ('e7', True), ('d8', True), ('g6', False), ('a1', False)]
-        piece_start = 'd7'
-        for test in white_test_dest:
-            self.assertEqual(game_logic.validate_move(white_game_dict, piece_start, test[0]), test[1])
-        for test in black_test_dest:
-            self.assertEqual(game_logic.validate_move(black_game_dict, piece_start, test[0]), test[1])
-
-    def test_no_clip_moves(self):
-        """ Test that can't clip enemy piece """
-        white_fen = '12/1b10/12/12/12/4b7/3bwb6/4b7 12 8 w 0 0 1'
-        white_dict = fen.deserialize_fen_string(self.white_fen)
-        black_fen = '12/1w10/12/12/12/4w7/3wbw6/4w7 12 8 b 0 0 1'
-        black_dict = fen.deserialize_fen_string(self.black_fen)
-        move_squares = ['c7', 'd6', 'd8', 'e7']
-        start_move = 'd7'
-        for square in move_squares:
-            self.assertFalse(game_logic.validate_move(white_dict, start_move, square))
-            self.assertFalse(game_logic.validate_move(black_dict, start_move, square))
-
-    def test_move_own_only(self):
-        """ Test that can't move opponent's pieces """
-        white_fen = '12/12/12/12/12/12/4w7/12 12 8 b 0 0 1'
-        black_fen = '12/12/12/12/12/12/4w7/12 12 8 w 0 0 1'
-        white_dict = fen.deserialize_fen_string(white_fen)
-        black_dict = fen.deserialize_fen_string(black_fen)
-        start_move = 'd7'
-        end_move = 'c7'
-        self.assertFalse(game_logic.validate_move(white_dict, start_move, end_move))
-        self.assertFalse(game_logic.validate_move(black_dict, start_move, end_move))
+        self.fn = game_logic.is_backwards
+# (move_start, move_end, active_player, board_width)
+    def test_values(self):
+        tests = [(False, (61, 75, 0, 12)), (False, (61, 62, 0, 12)), (False, (61, 60, 0, 12)), (True, (61, 47, 0, 12)),
+        (True, (61, 75, 1, 12)), (False, (61, 62, 1, 12)), (False, (61, 60, 1, 12)), (False, (61, 47, 1, 12))]
+        for test in tests:
+            self.assertEqual(test[0], self.fn(*test[1]))
 
 if __name__ == '__main__':
     unittest.main()
