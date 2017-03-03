adfcd392874f30c9920c6663701f1f8907c0fb00
svangordon <stephen.van.gordon@gmail.com>
2017-03-03 11:45:50 -0700

    include convert_alg_to_point

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,16 @@
-def validate_move(game_object, move_to, move_from=None):
-    """ Return Bool for whether a given move is legal """
-    if move_from is not None and game_object["gamePhase"] != 'm'
-            return False
-    elif move_from is not None game_object["gamePhase"] == 'd
+from string import ascii_lowercase
+
+# def validate_move(game_object, move_to, move_from=None):
+#     """ Return Bool for whether a given move is legal """
+#     if move_from is not None and game_object["gamePhase"] != 'm'
+#             return False
+#     elif move_from is not None game_object["gamePhase"] == 'd
+
+def convert_alg_to_point(coord, board_width, board_height):
+    col = int(ascii_lowercase.find(coord[0])) + 1 # dummy col
+    row = int(coord[1:])
+    if not 0 < col <= board_width:
+        raise ValueError('bad coord; invalid col in ' + coord)
+    if not 0 < row <= board_height:
+        raise ValueError('bad coord; invalid row in ' + coord)
+    return row*(board_width + 2) + col
--- game_logic_tests.py
+++ game_logic_tests.py
@@ -1,10 +1,63 @@
 import unittest
 import game_logic
 import fen
+import game_logic
 
-class TestMoveValidation(unittest.TestClass):
-    def test_no_move_from_in_deployment(self):
-        self.assertFalse(game_logic.validate_move(fen.create_fen(), 5, 5))
+class TestSquareConversion(unittest.TestCase):
+    """ make sure the game can convert algebraic coordinates to/from indices """
+    # def setUp(self):
+
+    def test_convert_algebraic_to_point(self):
+        square = 'e4'
+        board_width = 12
+        board_height = 8
+        resp = game_logic.convert_alg_to_point(square, board_width, board_height)
+        expected_resp = 61
+        self.assertEqual(expected_resp, resp)
+
+    def test_side_edge_alg_to_point(self):
+        square = 'a6'
+        board_width = 12
+        board_height = 8
+        resp = game_logic.convert_alg_to_point(square, board_width, board_height)
+        expected_resp = 85
+        self.assertEqual(expected_resp, resp)
+
+    def test_bottom_edge_alg_to_point(self):
+        square = 'f8'
+        board_width = 12
+        board_height = 8
+        resp = game_logic.convert_alg_to_point(square, board_width, board_height)
+        expected_resp = 118
+        self.assertEqual(expected_resp, resp)
+
+    def test_invalid_col(self):
+        square = 'm8'
+        board_width = 12
+        board_height = 8
+        with self.assertRaises(ValueError):
+            game_logic.convert_alg_to_point(square, board_width, board_height)
+        # expected_resp = None # maybe should raise?
+        # self.assertEqual(expected_resp, resp)
+
+    def test_invalid_row(self):
+        square = 'b9'
+        board_width = 12
+        board_height = 8
+        with self.assertRaises(ValueError):
+            game_logic.convert_alg_to_point(square, board_width, board_height)
+        # resp = game_logic.convert_alg_to_point(square, board_width, board_height)
+        # expected_resp = None # maybe should raise?
+        # self.assertEqual(expected_resp, resp)
+
+# class TestMoveValidation(unittest.TestClass):
+#     def setUp(self):
+#         self.one_piece_fen = '12/12/12/12/12/12/4w7/12 12 8 w 0 0 1'
+#         self.one_piece_dict = fen.deserialize_fen_string(self.one_piece_fen)
+#
+#     def test_white_move_forward(self):
+#         move =
+#         validation_resp = game
 
 if __name__ == '__main__':
     unittest.main()
