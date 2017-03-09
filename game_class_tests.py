44c83d78e6b9ab514184d9bc72e7dfcdc38543b6
svangordon <stephen.van.gordon@gmail.com>
2017-03-08 23:32:07 -0700

    uncomment tests

--- game_class_tests.py
+++ game_class_tests.py
@@ -2,30 +2,30 @@ import unittest
 from game_class import GameState
 from pprint import pprint
 
-# class TestDetermineDirection(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.game = GameState("standard", True)
-#         self.game.gen.piece('o', 30)
-#         # Order: NEWS (so that if you reverse the string, you swap which is in what directions)
-#         self.test_vals = [(30,16,0), (30,31,1), (30,29,2), (30,44,3)]
-#
-#     def test_determine_direction_n(self):
-#         """ should find eastward direction ok """
-#         test_vals = self.test_vals[0]
-#         self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
-#     def test_determine_direction_e(self):
-#         """ should find eastward direction ok """
-#         test_vals = self.test_vals[1]
-#         self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
-#     def test_determine_direction_s(self):
-#         """ should find eastward direction ok """
-#         test_vals = self.test_vals[2]
-#         self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
-#     def test_determine_direction_2(self):
-#         """ should find eastward direction ok """
-#         test_vals = self.test_vals[3]
-#         self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
+class TestDetermineDirection(unittest.TestCase):
+
+    def setUp(self):
+        self.game = GameState("standard", True)
+        self.game.gen.piece('o', 30)
+        # Order: NEWS (so that if you reverse the string, you swap which is in what directions)
+        self.test_vals = [(30,16,0), (30,31,1), (30,29,2), (30,44,3)]
+
+    def test_determine_direction_n(self):
+        """ should find eastward direction ok """
+        test_vals = self.test_vals[0]
+        self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
+    def test_determine_direction_e(self):
+        """ should find eastward direction ok """
+        test_vals = self.test_vals[1]
+        self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
+    def test_determine_direction_s(self):
+        """ should find eastward direction ok """
+        test_vals = self.test_vals[2]
+        self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
+    def test_determine_direction_2(self):
+        """ should find eastward direction ok """
+        test_vals = self.test_vals[3]
+        self.assertEqual(self.game.square(test_vals[0]).determine_direction(test_vals[1]), test_vals[2])
 
 class TestHandleMove(unittest.TestCase):
     def setUp(self):
