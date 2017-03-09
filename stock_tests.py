a9d6f160821f0cc381a484c23541a47755552c11
svangordon <stephen.van.gordon@gmail.com>
2017-03-09 14:40:57 -0700

    delete unneeded files

--- stock_tests.py
+++
@@ -1,5 +0,0 @@
-import hug
-import happy_birthday
-from pprint import pprint
-
-pprint(vars(hug.test.get(happy_birthday, 'happy_birthday', {'name': 'Timothy', 'age': 25}))) # Returns a Response object
